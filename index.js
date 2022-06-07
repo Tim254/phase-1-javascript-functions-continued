e79121803c1b47d5c61de04b66e3d7a686118334
Tim254 <tim.mailu@gmail.com>
2022-06-07 21:41:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(n = "roller-skate") {
+    return `This Saturday, I want to ${n}!`
+}
+function mondayWork(n = "go to the office"){
+    return `This Monday, I will ${n}.`
+}
+function wrapAdjective(n = "*") {
+    return function (adj = "a hard worker") {
+        return `You are ${n}${adj}${n}!`
+    }
+}
